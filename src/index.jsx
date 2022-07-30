2e138ff16ff06e0815454e82e96555f038503c2f
paulo.cardoso1 <paulo.cardoso@ztrax.com.br>
2022-07-30 16:22:02 -0300

    add: useReducer

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ExempleUseContext from './ExempleUseContext';
+import ExempleUseReducer from './ExempleUseReducer';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ExempleUseContext />
+    <ExempleUseReducer />
   </React.StrictMode>,
 );
